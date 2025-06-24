49d6a452d682c8b3e06de2e43d6c2764017d18f0
Hsinpa <paul.yc.hsin@viewsonic.com>
2025-06-24 09:34:55 +0800

    Re-enable function

--- main.py
+++ main.py
@@ -14,43 +14,35 @@ from src.utility.model_loader import ClassicILLMLoader
 async def main():
     load_dotenv()
 
-    sha = os.getenv('SHA')
     token = os.getenv('BOT_GH_TOKEN')
-
-    if len(sys.argv) < 2:
-        print("Usage: python my_script.py <patch_file>")
-        sys.exit(1)
 
     github_event_raw_json = sys.argv[1]
     github_event_json = json.loads(github_event_raw_json)
     patch_content = await fetch_github_patch(pull_request_url=github_event_json['pull_request']['url'], token=token)
 
-    print(patch_content)
+    sha = github_event_json['pull_request']['head']['sha']
+    comment_url = github_event_json['pull_request']['comments_url']
+    content_url = github_event_json['repository']['contents_url']
 
-    # try:
-    #     with open(patch_file, 'r') as file:
-    #         patch_lines = file.readlines()
-    #
-    #         filtered_p = ''.join(filter_patch(patch_lines))
-    #
-    #         # Get the custom instruction
-    #         c_instruction = fetch_github_file(content_url=content_url, file_path='pull_request_bot_instruction.txt',
-    #                           sha=sha, token=token)
-    #
-    #         agent = PRBotAgent(ClassicILLMLoader())
-    #         agent_graph = agent.create_graph()
-    #
-    #         feedback_content = agent_graph.invoke({
-    #             'pr_patch': filtered_p,
-    #             'custom_instruction': c_instruction,
-    #         },
-    #         {'run_name': 'PR Agent'})
-    #
-    #         send_github_comment(comment_url, feedback_content['plan'])
-    #
-    # except Exception as e:
-    #     print(f"Error reading {patch_file}: {e}")
-    #     sys.exit(1)
+    try:
+        # Get the custom instruction
+        c_instruction = fetch_github_file(content_url=content_url, file_path='pull_request_bot_instruction.txt',
+                          sha=sha, token=token)
+
+        agent = PRBotAgent(ClassicILLMLoader())
+        agent_graph = agent.create_graph()
+
+        feedback_content = agent_graph.invoke({
+            'pr_patch': patch_content,
+            'custom_instruction': c_instruction,
+        },
+        {'run_name': 'PR Agent'})
+
+        send_github_comment(comment_url, feedback_content['plan'])
+
+    except Exception as e:
+        print(f"Error reading {patch_content}: {e}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     asyncio.run(main())
