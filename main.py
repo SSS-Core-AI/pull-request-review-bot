7098e9746d7f88e64bcaa0e49879d9d43a79e2bd
Hsinpa <paul.yc.hsin@viewsonic.com>
2025-06-26 14:12:21 +0800

    remove try catch for cleaner error msg

--- main.py
+++ main.py
@@ -26,31 +26,27 @@ async def main(github_event_json: dict):
     self_repo_url = github_event_json['pull_request']['_links']['self']['href']
     file_repo_url = self_repo_url+'/files'
 
-    try:
-        # Get the custom instruction
-        c_instruction = await fetch_github_file(content_url=content_url, file_path='pull_request_bot_instruction.txt',
-                          sha=sha, token=token)
+    # Get the custom instruction
+    c_instruction = await fetch_github_file(content_url=content_url, file_path='pull_request_bot_instruction.txt',
+                      sha=sha, token=token)
 
-        commit_file_array = await fetch_github_files(file_repo_url, token=token)
+    commit_file_array = await fetch_github_files(file_repo_url, token=token)
 
-        pr_repo = PRAgentRepo(api_config, content_url, sha, token)
-        await pr_repo.preprocessing(commit_file_array)
+    pr_repo = PRAgentRepo(api_config, content_url, sha, token)
+    await pr_repo.preprocessing(commit_file_array)
 
-        pr_repo.run_pr_agent(patch_content=patch_content, c_instruction=c_instruction)
-        agent = PRBotAgent(ClassicILLMLoader(api_config))
-        agent_graph = agent.create_graph()
+    pr_repo.run_pr_agent(patch_content=patch_content, c_instruction=c_instruction)
+    agent = PRBotAgent(ClassicILLMLoader(api_config))
+    agent_graph = agent.create_graph()
 
-        feedback_content = agent_graph.invoke({
-            'pr_patch': patch_content,
-            'custom_instruction': c_instruction,
-        },
-        {'run_name': 'PR Agent'})
+    feedback_content = agent_graph.invoke({
+        'pr_patch': patch_content,
+        'custom_instruction': c_instruction,
+    },
+    {'run_name': 'PR Agent'})
 
-        send_github_comment(comment_url, feedback_content['plan'])
+    send_github_comment(comment_url, feedback_content['plan'])
 
-    except Exception as e:
-        print(f"Error reading {patch_content}: {e}")
-        sys.exit(1)
 
 if __name__ == "__main__":
     asyncio.run(main( json.loads(sys.argv[1]) ))
