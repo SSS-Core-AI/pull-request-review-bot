0d3a638b3e0e5f0fda2bc2b483dd217a694e0e59
Hsinpa <paul.yc.hsin@viewsonic.com>
2025-07-07 10:01:01 +0800

    Rename the custom instruction file v3

--- src/utility/static_variable.py
+++ src/utility/static_variable.py
@@ -1,2 +1,2 @@
-CUSTOM_INSTRUCTION_FILE = '.github/pr_agent/custom_instruction.txt'
-REPO_SUMMARY_FILE = '.github/pr_agent/repo_summary.txt'
+CUSTOM_INSTRUCTION_FILE = 'pr_agent_config/custom_instruction.txt'
+REPO_SUMMARY_FILE = 'pr_agent/repo_summary.txt'
