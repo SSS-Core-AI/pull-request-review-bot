f39a92d7c6cbff60e6df465fa4bfe7a7764c6a85
Hsinpa <paul.yc.hsin@viewsonic.com>
2025-07-11 11:00:23 +0800

    page to 1

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ async def process_comment(session_id: str, token: str, github_event_json: dict):
     comment_url = github_event_json['issue']['comments_url']
     repo_url = github_event_json['issue']['pull_request']['url']
 
-    page_comment_contents = await fetch_github_content(comment_url+"?per_page=10", token)
+    page_comment_contents = await fetch_github_content(comment_url+"?per_page=1", token)
     print('page_comment_contents', page_comment_contents['link_header'])
 
     comment_contents = (await fetch_github_content(comment_url, token))['data']
