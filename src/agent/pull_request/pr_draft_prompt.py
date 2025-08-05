1f0851eb12157b58b050a2b97751472f75c81f4a
Hsinpa <paul.yc.hsin@viewsonic.com>
2025-08-05 11:15:06 +0800

    Debug github comment v9
    Debug github comment v9

--- src/agent/pull_request/pr_draft_prompt.py
+++ src/agent/pull_request/pr_draft_prompt.py
@@ -35,9 +35,16 @@ PR_DRAFT_HUMAN_PROMPT = """\
 {custom_instruction}
 '''
 
-Focus on the potential issue from [PR PATCH], and used [File and its dependency] as supplement materials
+[How to set the line_number]
+'''
+Rule 1: If the file is delete, set the line_number to -1
+Rule 2: If the issue is a suggestion / improvement, not a coding bugs, set the line_number to -1
+Rule 3: If the issue is too huge, can not start from specific line, set the line_number to -1
 
-If the file is delete, set the line_number to -1
+In short, only set line_number, when you are absolutely sure where the line number locate, or else just set it to negative one.
+'''
+
+Focus on the potential issue from [PR PATCH], and used [File and its dependency] as supplement materials
 priority 'none' means this issue is not important, we shouldn't tackle on it
 
 Output the dependency file path in the format of json array as below
